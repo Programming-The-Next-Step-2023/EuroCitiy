e3890b5ce793c23f000c3e21b624637473ef6eb4
MadlenHoffstadt <80783145+MadlenHoffstadt@users.noreply.github.com>
2023-05-11 17:41:51 +0200

    add package skeleton

---
+++ R/living_expenses.R
@@ -0,0 +1,6 @@
+# GET LIVING EXPENSES
+#
+# Calling API to get data on living expenses, process raw data
+# Function to filter living expenses data by city
+
+
---
+++ R/ui.R
@@ -0,0 +1,30 @@
+# R Shiny User Interface
+#
+# Defining user interface for the "shall I move?" shiny app
+#
+
+#ui <- shiny::fluidPage(
+#
+#  # Introduction  -----------------------------
+#  shiny::titlePanel(title = "Shall I move?"),
+#
+#  # short intro text
+#  shiny::fluidRow(
+#    column(12,
+#           htmlOutput("introduction"))
+#  ),
+#
+#  shiny::br(),
+#  shiny::hr(),
+#
+#  # TAB 1 -----------------------------
+#  shiny::tabPanel("Compare two cities",
+#
+#  ),
+#
+#  # TAB 2 ----------------------------
+#  shiny::tabPanel("Find best city for you",
+#
+#
+#           )
+#  )
